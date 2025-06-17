f16147c65626e51967de3b1b6978fc6da47f677f
Maheswarichandrani <chandranimaheswari13@gmail.com>
2025-06-17 21:40:47 +0530

    EDA and Model_Training

--- src/logger.py
+++ src/logger.py
@@ -14,8 +14,10 @@ LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 # Configure logging
 logging.basicConfig(
+
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s: %(message)s",
     level=logging.INFO
+    
 )
 
