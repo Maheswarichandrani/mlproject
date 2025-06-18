6b01d5c12b19b7924f27a584f1738509e9d63234
Maheswarichandrani <chandranimaheswari13@gmail.com>
2025-06-18 14:49:52 +0530

    Hyper parameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,7 +50,48 @@ class ModelTrainer:
                 "Adaboost Regressor": AdaBoostRegressor()
             }
 
-            model_report :dict=evalute_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter': ['best', 'random'],
+                    'max_depth': [1,2,3,4,5,10,15,20,25],
+                    'max_features': ['auto','sqrt','log2'],
+                },
+                "Random Forest": {
+                    'max_depth': [5,8,15,'None',10],
+                    'min_samples_split': [2,8,15,20],
+                    'n_estimators': [100,200,500,1000],
+                    'max_features': [5,7,'auto',8]
+                },
+                "K-Neighbour Regressor": {
+                    "n_neighbors": [2, 3, 10, 20, 40, 50]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGB Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "catboost Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "Adaboost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+            model_report :dict=evalute_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models, param=params)
 
             ## To get the best model score
             best_model_score=max(sorted(model_report.values()))
